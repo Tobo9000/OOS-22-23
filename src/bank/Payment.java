7bd2dec3aecbda7d423642a072edd5a069b4c05d
Tobias <tschnuerpel@gmail.com>
2022-10-04 11:29:01 +0200

    Erste Klassen hinzugefügt

---
+++ src/bank/Payment.java
@@ -0,0 +1,12 @@
+package bank;
+
+public class Payment {
+
+    private String date;
+    private double amount;
+    private String description;
+
+    private double incomingInterest;
+    private double outgoingInterest;
+
+}
---
+++ src/bank/Transfer.java
@@ -0,0 +1,12 @@
+package bank;
+
+public class Transfer {
+
+    private String date;
+    private double amount;
+    private String description;
+
+    private String sender;
+    private String recipient;
+
+}
